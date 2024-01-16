854109f6ab987ce6e45f73ac31213f7a4ed3e052
leejaeyoon22 <tksska1514@gmail.com>
2024-01-16 10:36:39 +0900

    setting

---
+++ src/main/java/com/ll/sbjpa240116/SbJpa240116Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbjpa240116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpa240116Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJpa240116Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbjpa240116/SbJpa240116ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbjpa240116;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJpa240116ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
